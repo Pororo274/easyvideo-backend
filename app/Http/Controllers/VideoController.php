af6ca647a27a90af909cdab3016e4309615dd69a
Emil <emilzaripov48@gmail.com>
2024-05-03 08:27:15 +0300

    add videocontroller

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Asset;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class VideoController extends Controller
@@ -12,15 +13,17 @@ class VideoController extends Controller
     public function uploadChunk(Request $request): JsonResponse
     {
         $asset = Asset::query()->where('client_id', $request->input('clientId'))->first();
+        $chunk = $request->file('chunk');
+        $filepath = $asset->path ?? "assets/" . $chunk->hashName();
+
+        $path = Storage::disk('local')->path($filepath);
+        File::append($path, $chunk->get());
 
         if (is_null($asset)) {
-            $path = $request->file('chunk')->store('', 'editor-assets');
             $asset = Asset::query()->create([
                 'client_id' => $request->input('clientId'),
-                'path' => $path
+                'path' => $filepath
             ]);
-        } else {
-            Storage::append($asset->path, $request->file('chunk')->get());
         }
 
         return response()->json($asset);
